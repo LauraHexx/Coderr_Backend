d5ffccea4320224ef00eb3901cf1da896055a783
LauraHexx <l.hesidenz97@gmail.com>
2025-05-13 10:45:20 +0200

    added: admin panel

--- offers_orders_app/admin.py
+++ offers_orders_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Offer, OfferDetail, Order
+
+admin.site.register(Offer)
+admin.site.register(OfferDetail)
+admin.site.register(Order)
