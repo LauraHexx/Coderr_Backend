41d392b6232346e66dbceec505dcdfa5c65297ee
LauraHexx <l.hesidenz97@gmail.com>
2025-08-15 11:42:39 +0200

    updated: allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
+    "http://coderr.laura-hesidenz.de",
+    "https://coderr.laura-hesidenz.de"
 ]
 
 
